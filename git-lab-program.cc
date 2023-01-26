905e2fd48922f636727dec27af891b0718f2f6a0
Conner <cc257720@ohio.edu>
2023-01-25 19:55:23 -0500

    Hello git code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Conner Casey
+ *        Date: 1/25/23
+ * Description: First git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
